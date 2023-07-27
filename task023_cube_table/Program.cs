d2aab56df0347a10b6c186cf877ffe772c5c3e32
Sofia <rybina.sofia@gmail.com>
2023-07-27 14:23:13 +0300

    Cube table

---
+++ task023_cube_table/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine()!;
+    int result = Math.Abs(Convert.ToInt32(value));
+    Console.WriteLine();
+    return result;
+}
+
+void GetNum(int num)
+{
+    int power = 3; //степень
+    int i = 1;
+    Console.Write($"{num} -> ");
+    while(i <= num)
+    {
+        if(i == 1)
+        {
+            Console.Write(Math.Pow(i,power));
+            i++;
+        }
+        else
+        {
+            Console.Write(","+ " "+ Math.Pow(i,power));
+            i++;
+        }
+    }
+}
+
+int n = Prompt("Input N: ");
+GetNum(n);
